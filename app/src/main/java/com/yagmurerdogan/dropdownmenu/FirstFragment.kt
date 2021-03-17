c5b036343a5110dba753780aea0c4184549daabf
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-03-17 09:42:36 +0300

    Initial Commit

---
+++ app/src/main/java/com/yagmurerdogan/dropdownmenu/FirstFragment.kt
@@ -0,0 +1,37 @@
+package com.yagmurerdogan.dropdownmenu
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.yagmurerdogan.dropdownmenu.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onResume() {
+        super.onResume()
+        val languages = resources.getStringArray(R.array.languages)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, languages)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/yagmurerdogan/dropdownmenu/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yagmurerdogan.dropdownmenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.fragmentHolder,FirstFragment()).commit()
+    }
+}
